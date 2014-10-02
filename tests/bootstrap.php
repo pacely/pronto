c6d293634f2336f350cf70f83c8633d971dc4f92
ChrisRM <chris@hirvi.no>
2014-10-02 10:18:33 +0200

    Fixed path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
-$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader = require dirname(__DIR__).'/vendor/autoload.php';
 $loader->set('Tests', __DIR__);
 //require_once 'PHPUnit/Framework/Assert/Functions.php';
